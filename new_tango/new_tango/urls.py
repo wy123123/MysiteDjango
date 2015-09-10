e37a586078ecbaaffe7b41ab23af2fe706d72a7a
wy123123 <wangyu0917@hotmail.com>
2015-09-10 18:00:55 +0800

    Added change password function
    need to change the code that redirect to main page after successful
    password reset

--- new_tango/new_tango/urls.py
+++ new_tango/new_tango/urls.py
@@ -17,9 +17,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from registration.backends.simple.views import RegistrationView
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/password/change/done$', MyRegistrationView.as_view(), name='auth_password_change'),
     url(r'^accounts/', include('registration.backends.default.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
